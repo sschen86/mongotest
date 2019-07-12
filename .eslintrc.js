3dc95a182e268306706d0d051073f11945e10439
shushu.c <chenshushu@meeruu.com>
2019-07-12 18:03:29 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: '@smartx/eslint-config-tendative',
+    parserOptions: { parser: 'babel-eslint' }, // 使用 `babel` 编译的项目
+    rules: {
+      // 自定义规则
+    },
+  }
--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb')
 
-mongoose.connect('mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb');
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+    console.info('connect to db success')
 
-console.info({mongoose})
+    const memberSchema = mongoose.Schema({
+        nick: String,
+        createTime: Date,
+    })
+
+    memberSchema.methods.login = function () {
+        console.info('hi ' + this.nick)
+    }
+
+    const Member = mongoose.model('member', memberSchema)
+
+    Member.find({ }, function (err, data) {
+        console.info(err, data)
+    })
+
+    var zs = new Member({ nick: '张三', createTime: new Date() })
+
+    zs.save(function (err, that) {
+        if (err) return console.error(err)
+        that.login()
+    })
+
+    zs.login()
+})
+
+console.info({ mongoose })
